5c8bb3a673d2b197451dd077bb92d500cb0b8394
Shaariq Chundrigar <shaariqch@gmail.com>
2020-02-16 23:54:18 +1100

    Added models

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author' : 'ShaariqC',
-        'title' : 'Blog Post 1',
-        'content': 'First post',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author' : 'Haaziqc',
-        'title' : 'Blog Post 2',
-        'content': 'Second post',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
