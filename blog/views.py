55e4b4dfad6bd551dcf5e44dc45c464bd8fc7ed6
Shaariq Chundrigar <shaariqch@gmail.com>
2020-02-16 21:51:41 +1100

    Added templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author' : 'ShaariqC',
+        'title' : 'Blog Post 1',
+        'content': 'First post',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author' : 'Haaziqc',
+        'title' : 'Blog Post 2',
+        'content': 'Second post',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': "About"})
